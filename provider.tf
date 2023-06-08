8039c55cd82cb3dc0f1e389b59142271b28c8b4f
Vidhyakishore <30649286+VidhyaKishore@users.noreply.github.com>
2023-06-08 12:00:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  # region     = "us-west-1"
-  # access_key = "AKIA34LOMHJAS637MFMQ"
-  # secret_key = "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"
+  region     = "us-west-1"
+  access_key = "AKIA34LOMHJAS637MFMQ"
+  secret_key = "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"
 }
